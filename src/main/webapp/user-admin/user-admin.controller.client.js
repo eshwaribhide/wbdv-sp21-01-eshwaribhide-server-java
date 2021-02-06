498e8e4a00994bfb388a4b685249a296e296a2d9
Eshwari Bhide <ebhide99@gmail.com>
2021-02-06 13:11:10 -0800

    not displaying passwords

--- src/main/webapp/user-admin/user-admin.controller.client.js
+++ src/main/webapp/user-admin/user-admin.controller.client.js
@@ -51,7 +51,7 @@ function renderUsers(users) {
       .append(`
     <tr>
         <td>${user.username}</td>
-        <td>${user.password}</td>
+        <td></td>
         <td>${user.firstName}</td>
         <td>${user.lastName}</td>
         <td>${user.role}</td>
